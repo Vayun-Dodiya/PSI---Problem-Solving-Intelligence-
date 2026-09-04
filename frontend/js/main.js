c6496bf03804fcd1435150e629b3f1f93e8df55e
Vayun-Dodiya <vayundodiya919@gmail.com>
2026-09-05 00:24:47 +0530

    minor fixes - still working on it..

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,10 @@
 console.log("PSI Home Page Loaded");
+
+const problemInput = document.querySelector(".problem-input");
+
+problemInput.addEventListener("input", () => {
+    problemInput.style.height = "auto";
+    problemInput.style.height =
+        Math.min(problemInput.scrollHeight, 180) + "px";
+});
+
